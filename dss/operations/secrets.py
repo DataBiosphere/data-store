2cf16ec9d056442c51a640c60bf432f2a135ae00
DailyDreaming <lblauvel@ucsc.edu>
2020-02-03 11:26:58 -0800

    Clean up.

--- dss/operations/secrets.py
+++ dss/operations/secrets.py
@@ -17,8 +17,6 @@ from dss.operations import dispatch
 from dss.operations.util import polite_print
 from dss.util.aws.clients import secretsmanager as sm_client  # type: ignore
 
-
-# sm_client = boto3.client('ssm', region_name='us-west-2')
 logger = logging.getLogger(__name__)
 
 
