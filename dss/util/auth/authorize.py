0d96d44635ea6ad802725178257f70ecfacd5737
amar jandu <ajandu@ucsc.edu>
2020-02-20 08:54:13 -0800

    removing extra comments

--- dss/util/auth/authorize.py
+++ dss/util/auth/authorize.py
@@ -3,20 +3,11 @@ class Authorize:
     def __init__(self):
         pass
 
-    # name this better
     def security_flow(self, authz_methods, *args, **kwargs):
         """
         This function maps out flow for a given security config
         """
 
-        # def process_keys(self):
-        #     raise NotImplementedError()
-        #
-        # def __call__(self, argv: typing.List[str], args: argparse.Namespace):
-        #     self.process_keys()
-
-        # there has to be a way to replicate this to call the applicable security flow, perhaps the
-        # hca blob-store has more of an example on how to perform this
         raise NotImplementedError()
 
     def assert_required_parameters(self, provided_params: dict, required_params: list):
