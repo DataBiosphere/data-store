c5b545a298bdaa4958e3a95a01081202142b9047
amar jandu <ajandu@ucsc.edu>
2020-02-21 16:41:28 -0800

    cleaner kwargs validation

--- dss/util/auth/authorize.py
+++ dss/util/auth/authorize.py
@@ -23,15 +23,10 @@ class Authorize(metaclass=AuthRegistry):
         This function maps out flow for a given security config
         Be sure to call it from the derived class to ensure the JWT has groups set
         """
+        self.assert_required_parameters(kwargs,['groups', 'token'])
         groups = kwargs.get('security_groups')
         token = kwargs.get('security_token')
-        if groups is not None and token is not None:
-            self.assert_authorized_group(groups, token)
-        else:
-            title = "security_groups not found"
-            err = f'Error with jwt group validation, unable to locate required security groups,'
-            err += f' please ensure kwarg: "security_groups"" is set within the assert_security decorator'
-            raise DSSException(500, title, err)
+        self.assert_authorized_group(groups, token)
 
     def assert_required_parameters(self, provided_params: dict, required_params: list):
         """
@@ -42,7 +37,9 @@ class Authorize(metaclass=AuthRegistry):
         """
         for param in required_params:
             if param not in provided_params:
-                raise Exception('unable to use Authorization method, missing required parameters')
+                title = "Missing Security Paramters"
+                err = f'Missing parameters within {provided_params.keys()}, unable to locate {param},'
+                raise DSSException(500, title, err)
         return
 
     def assert_authorized_group(self, group: typing.List[str], token: dict) -> None:
