65cdf5a3b5f14862d45f99786485b08c8af35061
Charles Reid <chmreid@ucsc.edu>
2020-02-27 14:06:58 -0800

    lint

--- dss/util/auth/__init__.py
+++ dss/util/auth/__init__.py
@@ -3,6 +3,6 @@ import logging
 from .wrapper import AuthWrapper
 from .authorize import Authorize
 from .fusillade import Fusillade
-#from .auth0 import Auth0
+# from .auth0 import Auth0
 
 logger = logging.getLogger(__name__)
