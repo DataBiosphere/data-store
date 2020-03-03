f236d9f4745fdd464c80bfc3e033f1b5d0a47ec3
amar jandu <ajandu@ucsc.edu>
2020-03-03 12:37:51 -0800

    list of lists, string set

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -259,13 +259,15 @@ class TestDynamodbUtils(unittest.TestCase):
         formatted_item_attributes = {"sort_key": "02434574-dbec-45dd-8bc5-d5dae7007780.2020-02-24T201715.601067Z",
                                      "hash_key": "travis-test@platform-hca.iam.gserviceaccount.com",
                                      "body": "owner",
-                                     "list": ["GMAW", "FCAW", "GTAW"]}
+                                     "string_set": ["GMAW", "FCAW", "GTAW"],
+                                     "list": [{"S": "Cookies"}, {"S": "Coffee"}, {"N", "3.14159"}]}
 
         def mock_ddb_call():
             return {"Item": {"sort_key": {"S": "02434574-dbec-45dd-8bc5-d5dae7007780.2020-02-24T201715.601067Z"},
                              "hash_key": {"S": "travis-test@platform-hca.iam.gserviceaccount.com"},
                              "body": {"S": "owner"},
-                             "list": {"SS": ["GMAW", "FCAW", "GTAW"]}}}
+                             "string_set": {"SS": ["GMAW", "FCAW", "GTAW"]},
+                             "list": {"L": [{"S": "Cookies"}, {"S": "Coffee"}, {"N", "3.14159"}]}}}
 
         with mock.patch('dss.dynamodb.db.get_item') as mock_get_item:
             mock_get_item.return_value = mock_ddb_call()
